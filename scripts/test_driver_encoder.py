19a017c5074057c321d2ff9c4d699f4ee70cfb7e
Juan David Serrano <jd.serranom@uniandes.edu.co>
2020-07-19 19:06:21 -0500

    A

--- scripts/test_driver_encoder.py
+++ scripts/test_driver_encoder.py
@@ -70,7 +70,7 @@ def test_driver_encoder():
         # print("Speed: " + str(speed) + " mm/s")
         rate.sleep()
     # print(data[0:3])
-    with open('data.csv', 'w', newline='') as file:
+    with open('data.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerows(data)
     print("Stopped")
