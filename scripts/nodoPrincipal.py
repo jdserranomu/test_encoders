d15f0c56bb8db6e85e901bb75b4d6ab2d011bb71
maescalante <ma.escalante@uniandes.edu.co>
2020-07-30 13:11:39 -0500

    nodoPrinciapl

---
+++ scripts/nodoPrincipal.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import rospy
+import socket
+import time
+import numpy as np
+from std_msgs.msg import Int32
+from std_msgs.msg import String
+from geometry_msgs.msg import Pose
+#from master_msgs_iele3338.msg import Obstacle
+#from master_msgs_iele3338.srv import StartService, EndService
+from test_encoders.srv import StartService
+from test_encoders.msg import *
+
+import os
+
+#Variable con toda la informacion del escenario
+escenario = None
+# Posicion de inicio del robot
+start = Pose()
+# Posicion final a la que se quiere llegar
+goal = Pose()
+# Numero de obstaculos que se envian
+n_obstacles = 0
+# Objeto tipo Obstacle con informacion de los obstaculos
+obstacles_array = []
+# Contrasena obtenida
+password = None
+# Variable que identifica si se termino el recorrido
+esperarTerminarRecorrido = False
+# Variable que identifica que se solicito el servicio start_service
+esperarStartService = False
+
+positionsMap=[]
+
+def createMap():
+    tam=250
+    cte=500
+    global positionsMap
+    for i in range(4):
+        pos=Pose()
+        pos.position.x=cte*i+tam/2
+        pos.position.y=tam/2
+        pos.orientation.w=0
+        positionsMap.append(pos)
+    for i in range(4):
+        pos=Pose()
+        pos.position.x=cte*i+tam+tam/2
+        pos.position.y=1000-tam/2
+        pos.orientation.w=0
+        positionsMap.append(pos)
+def leviathan():
+
+    rospy.init_node('nodoPrincipal', anonymous=True)  # inicializa el nodo
+    pubEstado = rospy.Publisher('estado', Int32, queue_size=10)
+    pubEstado.publish(0)  # Aca publica que esta esperando ack_service 0
+
+    try:
+
+        start_service(1,5,0,[])
+        rospy.wait_for_service('iniciar_recorrido')
+        rospy.wait_for_service('iniciar_odometria')
+        rospy.wait_for_service('iniciar_encoders')
+
+        iniciar_service = rospy.ServiceProxy('iniciar_recorrido', StartService)  # Crea el objeto referente al servicio
+        iniciar_service(escenario)
+        iniciar_odometria = rospy.ServiceProxy('iniciar_odometria', StartService)
+        iniciar_odometria(escenario)
+        iniciar_encoders = rospy.ServiceProxy('iniciar_encoders', StartService)
+        iniciar_encoders(escenario)
+        # rospy.Subscriber ('termino_recorrido', Int32, handle_terminar_recorrido)
+        s1=rospy.Service('terminar_control', TerminarRecorrido, handle_terminar_recorrido)
+        rospy.loginfo("Esperando terminar control")
+        while not esperarTerminarRecorrido:
+            pass
+        pubEstado.publish(4)
+
+
+
+    except rospy.ServiceException:
+        print("Ocurrio un error solicitando servicio")
+
+
+def start_service(start,end,n_obs,obs):
+    global positionsMap
+    startS=StartService()
+    startS.start=positionsMap[start]
+    startS.goal=positionsMap[end]
+    startS.n_obstacles=n_obs
+    startS.obstacles=obs
+
+    escenario = startS
+    start = startS.start
+    goal = startS.goal
+    n_obstacles = startS.n_obstacles
+    obstacles_array = startS.obstacles
+
+
+    return []
+
+
+def handle_terminar_recorrido(req):
+    global esperarTerminarRecorrido
+    esperarTerminarRecorrido = True
+    rospy.loginfo ("Recibio el llamado del topico para terminar recorrido")
+    return []
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        leviathan()
+    except rospy.ROSInterruptException:
+        pass
